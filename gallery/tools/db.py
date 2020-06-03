0e4a4372503e9be4b757801ea1feca5df1e827b4
C. David Shaffer <cdshaffer@acm.org>
2020-06-03 19:35:21 +0000

    Use secrets for db access

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -1,22 +1,29 @@
 import psycopg2
-
-db_host = "demo-database-1.c923ckbw7nvl.us-east-2.rds.amazonaws.com"
-db_name = "image_gallery"
-db_user = "image_gallery"
-
-password_file = "/home/ec2-user/.image_gallery_config"
+import json
+from secrets import get_secret_image_gallery
 
 connection = None
 
-def get_password():
-    f = open(password_file, "r")
-    result = f.readline()
-    f.close()
-    return result[:-1]
+def get_secret():
+    jsonString = get_secret_image_gallery()
+    return json.loads(jsonString)
+
+def get_password(secret):
+    return secret['password']
+
+def get_host(secret):
+    return secret['host']
+
+def get_username(secret):
+    return secret['username']
+
+def get_dbname(secret):
+    return secret['database_name']
 
 def connect():
     global connection
-    connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
+    secret = get_secret()
+    connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret), password=get_password(secret))
 
 def execute(query,args=None):
     global connection
@@ -29,10 +36,6 @@ def execute(query,args=None):
 
 def main():
     connect()
-    res = execute('select * from users')
-    for row in res:
-        print(row)
-    res = execute("update users set password=%s where username='fred'", ('banana',))
     res = execute('select * from users')
     for row in res:
         print(row)
